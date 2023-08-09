4329e73cdda2d642ccfdfa64e58dcce6ea3b4981
Abasay <abdulsalamasheem@gmail.com>
2023-08-09 09:33:57 -0700

    fix webstack

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+# Puppet manifest to fix why apache is returning 500 error
+
+exec { 'fix the php extension issue':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
+}
