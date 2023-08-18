c4f9c097fd9643b9a594f7b1ed60ea32ffd08fe5
Abasay <abdulsalamasheem@gmail.com>
2023-08-17 20:36:27 -0700

    webstack debugging

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,13 @@
+#fix for nginx
+
+exec {'fix-for-nginx':
+     command => 'sed -i "s@usr/share/nginx/html@var/www/html@g" /etc/nginx/sites-available/default',
+     path => '/usr/local/bin/:/bin/',
+     before => Exec['restart-nginx'] 
+}
+
+#Restart nginx
+exec {'restart-nginx':
+     command => 'nginx restart',
+     path => '/etc/init.d/'
+}
