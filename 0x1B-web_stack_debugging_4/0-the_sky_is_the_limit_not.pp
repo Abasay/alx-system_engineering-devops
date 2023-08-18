db2bc446b021deda20c5348f5d13547caeeb11bc
Abasay <abdulsalamasheem@gmail.com>
2023-08-18 07:33:23 -0700

    editted

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,13 +1,13 @@
 #fix for nginx
 
 exec {'fix-for-nginx':
-     command => 'sed -i "s@usr/share/nginx/html@var/www/html@g" /etc/nginx/sites-available/default',
-     path => '/usr/local/bin/:/bin/',
-     before => Exec['restart-nginx'] 
+  command => 'sed -i "s@usr/share/nginx/html@var/www/html@g" /etc/nginx/site11;rgb:1d1d/2020/2121s-available/default',
+  path    => '/usr/local/bin/:/bin/',
+  before  => Exec['restart-nginx']
 }
 
 #Restart nginx
 exec {'restart-nginx':
-     command => 'nginx restart',
-     path => '/etc/init.d/'
+  command => 'nginx restart',
+  path    => '/etc/init.d/'
 }
