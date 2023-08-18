f07d4d0256e72c8e2f4245ab08148d9cb4ae1e82
Abasay <abdulsalamasheem@gmail.com>
2023-08-18 07:38:34 -0700

    sky is the limit

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,7 +1,7 @@
 #fix for nginx
 
 exec {'fix-for-nginx':
-  command => 'sed -i "s@usr/share/nginx/html@var/www/html@g" /etc/nginx/site11;rgb:1d1d/2020/2121s-available/default',
+  command => 'sed -i "s@usr/share/nginx/html@var/www/html@g" /etc/nginx/sites-available/default',
   path    => '/usr/local/bin/:/bin/',
   before  => Exec['restart-nginx']
 }
